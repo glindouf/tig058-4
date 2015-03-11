a43da38de47361ce327548964cb2cfd2082edd86
johannesleander <leanderjohannes@gmail.com>
2015-03-11 19:35:08 +0100

    Added basic GUI-code

--- src/club/manager/gui/Window.java
+++ src/club/manager/gui/Window.java
@@ -1,11 +1,33 @@
-package club.manager.gui;
+//package club.manager.gui;
 
+import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-/**
- *
- * @author phcr
- */
 public class Window extends JFrame {
+
+    public Window()	{
+        initUI();
+    }
+
+    private void initUI()	{
+
+        setTitle("");
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
     
+    
+
+    public static void main (String [] args) {
+
+        EventQueue.invokeLater(new Runnable() {
+            
+            @Override
+            public void run() {
+                Window ex = new Window();
+                ex.setVisible(true);
+            }
+        });
+    }
 }
