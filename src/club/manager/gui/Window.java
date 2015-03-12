e441ce1cbd1c14d381563bf7056a621e34fac734
johannesleander <leanderjohannes@gmail.com>
2015-03-12 20:01:43 +0100

    Simplified GUI code

--- src/club/manager/gui/Window.java
+++ src/club/manager/gui/Window.java
@@ -1,33 +1,36 @@
-//package club.manager.gui;
+package club.manager.gui;
 
-import java.awt.EventQueue;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Window extends JFrame {
-
+public class Window extends JFrame {   
+    
+    JButton reg = new JButton("Registervård");
+    JButton search = new JButton("Registervård");
+    
+    /*
+    Passed contents of constructor to separate method "initUI"
+    */
     public Window()	{
         initUI();
     }
-
-    private void initUI()	{
-
-        setTitle("");
+    
+    /*
+    Elements used in constructor
+    */
+    public final void initUI()	{
+     
+        JPanel panel = new JPanel();   
+        
+        setTitle("Club Manager");
         setSize(600, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-    
-    
-
-    public static void main (String [] args) {
-
-        EventQueue.invokeLater(new Runnable() {
-            
-            @Override
-            public void run() {
-                Window ex = new Window();
-                ex.setVisible(true);
-            }
-        });
+        
+        panel.add(reg);
+        panel.add(search);
+        
+        this.add(panel);
     }
 }
