f9d8d242dc2ee89b7483ed74469f8389dde85e05
Philip <philipmayoo@gmail.com>
2015-03-16 11:45:53 +0100

    Get all fields from member

--- src/club/db/Query.java
+++ src/club/db/Query.java
@@ -1,10 +1,13 @@
 package club.db;
 
 import club.domain.Member;
+import club.domain.Team;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
 
 /**
  *
@@ -14,6 +17,19 @@ public class Query {
     
     static public Connector connector;
 
+    public static Member memberFromRS(ResultSet rs) throws SQLException {
+        Member m = new Member();
+        m.setId(rs.getString("id"));
+        m.setGivenname(rs.getString("givenname"));
+        m.setSurname(rs.getString("surname"));
+        m.setEmail(rs.getString("email"));
+        m.setGender(rs.getInt("gender"));
+        m.setBirthdate(rs.getLong("birthdate"));
+        m.setJoindate(rs.getLong("joindate"));
+        m.setActive(rs.getBoolean("active"));
+        return m;
+    }
+    
     public static Member getMemberWithId(String id) {
        Member m = new Member();
        try {
@@ -22,14 +38,7 @@ public class Query {
            ResultSet rs = stmnt.executeQuery(query);
            
            while (rs.next()) {
-               m.setId(rs.getString("id"));
-               m.setGivenname(rs.getString("givenname"));
-               m.setSurname(rs.getString("surname"));
-               m.setEmail(rs.getString("email"));
-               m.setGender(rs.getInt("gender"));
-               m.setBirthdate(rs.getLong("birthdate"));
-               m.setJoindate(rs.getLong("joindate"));
-               m.setActive(rs.getBoolean("active"));
+               m = memberFromRS(rs);
            }
        } catch (SQLException e) {
            System.out.println(e);
@@ -98,21 +107,80 @@ public class Query {
    
             ResultSet rs = stmnt.executeQuery(query);
             while (rs.next()) {
-                Member m = new Member();
-                m.setId(rs.getString("id"));
-                m.setGivenname(rs.getString("givenname"));
-                m.setSurname(rs.getString("surname"));
-                m.setEmail(rs.getString("email"));
-                m.setGender(rs.getInt("gender"));
-                m.setBirthdate(rs.getLong("birthdate"));
-                m.setJoindate(rs.getLong("joindate"));
-                m.setActive(rs.getBoolean("active"));
-                am.add(m);
+                am.add(memberFromRS(rs));
             }
         } catch (SQLException e) {
             System.out.println(e);
         }        
         return am;
+    }
+    
+    public static ArrayList<Member> getChildrenForMember (String id) {
+        ArrayList<Member> am = new ArrayList<Member>();
+        
+        try {
+            Statement stmnt = connector.connection.createStatement();
+            String query = String.format("SELECT member.* FROM member" 
+                    + " join(select child.id from child where child.parentid='%s') as cid"
+                    + " on (cid.id=member.id)", id);
+   
+            ResultSet rs = stmnt.executeQuery(query);
+            while (rs.next()) {
+                am.add(memberFromRS(rs));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }        
+        return am;
+    }
+    
+    public static ArrayList<Member> getParentsForMember (String id) {
+        ArrayList<Member> am = new ArrayList<Member>();
+        
+        try {
+            Statement stmnt = connector.connection.createStatement();
+            String query = String.format("SELECT member.* FROM member" 
+                    + " join(select parent.id from parent where parent.childid='%s') as pid"
+                    + " on (pid.id=member.id)", id);
+   
+            ResultSet rs = stmnt.executeQuery(query);
+            while (rs.next()) {
+                am.add(memberFromRS(rs));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }        
+        return am;
+    }
+       
+    
+    public static Dictionary getAllMemberId(String id) {
+        Dictionary d = new Hashtable();
+        Member m = new Member();
+        Team t = new Team();
+        ArrayList<Member> children;
+        ArrayList<Member> parents;
+        ArrayList<Member> coach;
+        
+        
+        m = getMemberWithId(id);
+        
+        if (isChild(id)) {
+            parents = getParentsForMember(id);
+        } else if (isParent(id)) {
+            //children = getChildrenForMember(id);
+        } else if (isCoach(id)) {
+            //coach = getCoachTeams(id);
+        }
+        
+        d.put("member", m);
+        d.put("team", t);
+        d.put("parents", p);
+        d.put("children", c);
+        d.put("coach", c);
+        
+        
+        return d;
     }
     
 }
