25326bb1cc078815d374f2c229d68bfc95dbb591
johannesleander <leanderjohannes@gmail.com>
2015-03-16 07:34:08 +0100

    Added elements in Registry

--- src/club/manager/gui/Search.java
+++ src/club/manager/gui/Search.java
@@ -1,12 +1,10 @@
 
 package club.manager.gui;
 
-import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
 /**
  *
@@ -29,9 +27,9 @@ public class Search extends JFrame implements ActionListener{
     public final void initUI() {   
         setTitle("Search");
         setVisible(true); 
-        setSize(600, 400);
+        //setSize(600, 400);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
                  
     }
     
--- src/club/manager/gui/Window.java
+++ src/club/manager/gui/Window.java
@@ -1,13 +1,11 @@
 package club.manager.gui;
 
-import java.awt.Container;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.Container;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import javax.swing.JButton;
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -15,24 +13,31 @@ import javax.swing.JPanel;
 public class Window extends JFrame {
 
     public Window() {
-
-        initUI();
+        initWindow();
     }
 
-    private void initUI() {
+    /*
+    All the statements used in constructor.
+    */
+    private void initWindow() {
         
-        JPanel panel = new JPanel(new BorderLayout());
-        
+        JPanel panel  = new JPanel(new BorderLayout(100, 100));
+        JPanel fPanel = new JPanel(new FlowLayout());
+                 
         JButton registryBtn = new JButton("Registry");
         JButton searchBtn   = new JButton("Search");
-                       
-        panel.add(registryBtn, BorderLayout.WEST);
-        panel.add(searchBtn, BorderLayout.EAST);
+        
+        panel.add(fPanel, BorderLayout.NORTH);
+        fPanel.add(registryBtn);
+        fPanel.add(searchBtn);       
         
         add(panel);
         
         pack();
         
+        /*
+        ActionListener for Registry Button.
+        */
         registryBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
@@ -41,24 +46,29 @@ public class Window extends JFrame {
             }
         });
         
+        /*
+        ActionListener for Search Button.
+        @param 
+        */
         searchBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
                 Search srch = new Search();
             }
         });
-       
-
+        
         setTitle("Club");
         setSize(600, 400);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         
     }
-
+    /*
+    Called upon to start the GUI.
+    */
     public static void StartGUI() {
 
-        EventQueue.invokeLater(new Runnable() {
+       EventQueue.invokeLater(new Runnable() {
         
             @Override
             public void run() {
