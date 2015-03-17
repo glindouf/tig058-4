48b8fa0149701890c0ed15b9f013b9bfa2b0150f
Philip <philipmayoo@gmail.com>
2015-03-17 08:56:23 +0100

    Removed test code

--- src/club/Club.java
+++ src/club/Club.java
@@ -21,25 +21,7 @@ public class Club {
      */
     public static void main(String[] args) {
         try {
-            Connector.connection = DriverManager.getConnection("jdbc:sqlite:club.db"); 
-            Member m = Query.getMemberWithId("000101-9213");
-            
-            System.out.println(Query.isParent(m.getId()));
-            System.out.println(Query.isChild(m.getId()));
-            System.out.println(Query.isCoach(m.getId()));
-            
-            ArrayList<Member> parents = Query.getParentsForMember(m.getId());            
-            for (Member p : parents) {
-                System.out.println(p.toString());
-            }
-            
-            
-            System.out.println("D> " + Query.getAllMemberId(m.getId()));
-            
-            Collections.sort(Query.getAllMembers(), new MemberSurnameComparator());
-            
-            System.out.println(Query.getMembersAndCoachFromTeam("D"));
-            
+            Connector.connection = DriverManager.getConnection("jdbc:sqlite:club.db");            
         } catch (SQLException e) {
             System.out.println(e);
         }            
